5df8f55c85b9cff080b25d6f2a39e480302e0e66
Silver <Silverg33k@gmail.com>
2023-08-27 16:17:06 +0100

    test: added jest and supertest package for testing

---
+++ tests/index.test.ts
@@ -0,0 +1,20 @@
+const request = require("supertest");
+const app = require('../src/index');
+
+describe("Check is its running", () => {
+    test("GET /", (done) => {
+      request(app)
+        .get("/")
+        .expect("Content-Type", /json/)
+        .expect(200)
+    });
+});
+
+describe("Check top 3", () => {
+  test("GET /top3", (done) => {
+    request(app)
+      .get("/")
+      .expect("Content-Type", /json/)
+      .expect(200)
+  });
+});
